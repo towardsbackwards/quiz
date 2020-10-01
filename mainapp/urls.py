cba37853a1cc09de4f11f7103adfa44890893dbd
towardsbackwards <Protuberance23>
2020-10-01 21:14:46 +0300

    interim commit for passed quizzes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import QuizView, IndexView
+from .views import QuizView, IndexView, PassedQuizzesView
 
 app_name = 'mainapp'
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('quiz/<int:quiz_pk>', QuizView.as_view(), name='quiz'),
+    path('quiz/<int:pk>', QuizView.as_view(), name='quiz'),
+    path('my_quizzes/<int:pk>', PassedQuizzesView.as_view(), name='passed')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,24 @@
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView, TemplateView, DetailView
 from ipware import get_client_ip
 from mainapp.forms import QuizForm
-from mainapp.models import Quiz
+from mainapp.models import Quiz, Question, Answer
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
     active_quizzes = Quiz.objects.filter(active=True)
     extra_context = {'active_quizzes': active_quizzes}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_id = self.request.user.id
+        ip, is_routable = get_client_ip(self.request)
+        if self.request.user.is_authenticated:
+            user_passed_quizzes = Quiz.objects.filter(question__answer__user_id=user_id).distinct()
+        else:
+            user_passed_quizzes = Quiz.objects.filter(question__answer__user_ip=ip).distinct()
+        context['user_passed_quizzes'] = user_passed_quizzes
+        return context
 
 
 class QuizView(FormView):
@@ -17,18 +28,28 @@ class QuizView(FormView):
 
     def get_form_kwargs(self):
         user = self.request.user
-        quiz_pk = self.kwargs.get('quiz_pk', None)
+        pk = self.kwargs.get('pk', None)
         ip, is_routable = get_client_ip(self.request)
         form_kwargs = super(QuizView, self).get_form_kwargs()
         form_kwargs['user'] = user
-        form_kwargs['quiz_pk'] = quiz_pk
+        form_kwargs['quiz_pk'] = pk
         form_kwargs['ip'] = ip
         return form_kwargs
 
     def form_valid(self, form):
-        # if ip not in str(VotersIP.objects.filter(poll_id=poll_id).values('ip')):
-        #     store_ip(ip, poll_id)
-        # else:
-        #     return HttpResponse(f'С данного ip-адреса уже голосовали в этом опросе!')
         form.save()
         return super(QuizView, self).form_valid(form)
+
+
+class PassedQuizzesView(DetailView):
+    model = Quiz
+    template_name = 'passed_quizzes.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        quiz_pk = self.kwargs.get('pk')
+        quiz_questions = Question.objects.filter(from_quiz_id=quiz_pk)
+        quiz_answers = Answer.objects.filter(answered_question__from_quiz_id=quiz_pk)
+        context['quiz_questions'] = quiz_questions
+        context['quiz_answers'] = quiz_answers
+        return context
