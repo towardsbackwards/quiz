03e9515543d6ddbc4132849b31849a97858f6216
towardsbackwards <Protuberance23>
2020-10-03 16:09:36 +0300

    REST

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,8 +6,8 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('quiz/', ActiveQuizzesView.as_view(), name='active_quizzes'),
-    path('quiz/<int:pk>', ActiveQuizView.as_view(), name='active_quiz'),
+    path('active_quizzes/', ActiveQuizzesView.as_view(), name='active_quizzes'),
+    path('active_quizzes/<int:pk>', ActiveQuizView.as_view(), name='active_quiz'),
     path('my_quizzes/', PassedQuizzesView.as_view(), name='passed_quizzes'),
     path('my_quizzes/<int:pk>', PassedQuizView.as_view(), name='passed_quiz')
 ]
