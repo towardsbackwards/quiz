dee6b44f21c8593eaf01eb0fb9fbd2bf38b8ac94
towardsbackwards <Protuberance23>
2020-10-03 16:01:23 +0300

    REST

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import QuizView, IndexView, PassedQuizzesView
+from .views import PassedQuizzesView, IndexView, PassedQuizView, ActiveQuizzesView, ActiveQuizView
 
 app_name = 'mainapp'
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('quiz/<int:pk>', QuizView.as_view(), name='quiz'),
-    path('my_quizzes/<int:pk>', PassedQuizzesView.as_view(), name='passed')
+    path('quiz/', ActiveQuizzesView.as_view(), name='active_quizzes'),
+    path('quiz/<int:pk>', ActiveQuizView.as_view(), name='active_quiz'),
+    path('my_quizzes/', PassedQuizzesView.as_view(), name='passed_quizzes'),
+    path('my_quizzes/<int:pk>', PassedQuizView.as_view(), name='passed_quiz')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import FormView, TemplateView, DetailView
+from django.views.generic import FormView, TemplateView, DetailView, ListView
 from ipware import get_client_ip
 from mainapp.forms import QuizForm
 from mainapp.models import Quiz, Question
@@ -11,29 +11,42 @@ class IndexView(TemplateView):
     active_quizzes = Quiz.objects.filter(active=True)
     extra_context = {'active_quizzes': active_quizzes}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+
+class ActiveQuizzesView(ListView):
+    """Отображение списка активных опросов"""
+    model = Quiz
+    template_name = 'active_quizzes_list.html'
+
+    def get_queryset(self):
+        return Quiz.objects.filter(active=True)
+
+
+class PassedQuizzesView(ListView):
+    """Отображение списка пройденных опросов"""
+    model = Question
+    template_name = 'passed_quizzes_list.html'
+
+    def get_queryset(self):
         user_id = self.request.user.id
         ip, is_routable = get_client_ip(self.request)
         if self.request.user.is_authenticated:
             user_passed_quizzes = Quiz.objects.filter(question__answer__user_id=user_id).distinct()
         else:
             user_passed_quizzes = Quiz.objects.filter(question__answer__user_ip=ip).distinct()
-        context['user_passed_quizzes'] = user_passed_quizzes
-        return context
+        return user_passed_quizzes
 
 
-class QuizView(FormView):
+class ActiveQuizView(FormView):
     """Контроллер отоборажения формы опроса"""
     form_class = QuizForm
-    template_name = 'start_quiz.html'
+    template_name = 'active_quiz.html'
     success_url = '/'
 
     def get_form_kwargs(self):
         user = self.request.user
         pk = self.kwargs.get('pk', None)
         ip, is_routable = get_client_ip(self.request)
-        form_kwargs = super(QuizView, self).get_form_kwargs()
+        form_kwargs = super(ActiveQuizView, self).get_form_kwargs()
         form_kwargs['user'] = user
         form_kwargs['quiz_pk'] = pk
         form_kwargs['ip'] = ip
@@ -41,13 +54,13 @@ class QuizView(FormView):
 
     def form_valid(self, form):
         form.save()
-        return super(QuizView, self).form_valid(form)
+        return super(ActiveQuizView, self).form_valid(form)
 
 
-class PassedQuizzesView(DetailView):
+class PassedQuizView(DetailView):
     """Контроллер отображения пройденных опросов с ответами"""
     model = Quiz
-    template_name = 'passed_quizzes.html'
+    template_name = 'passed_quiz.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -55,3 +68,4 @@ class PassedQuizzesView(DetailView):
         quiz_questions = Question.objects.filter(from_quiz_id=quiz_pk)
         context['quiz_questions'] = quiz_questions
         return context
+
