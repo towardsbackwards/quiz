efdaaeb75e52a6d4a9cbe97b4ca5397410238e0d
towardsbackwards <Protuberance23>
2020-10-03 03:07:06 +0300

    Single choice JQuery - preventing human factor errors / disabling other checkboxes complete cases

--- static/js/disable_checkbox.js
+++ static/js/disable_checkbox.js
@@ -1,25 +1,3 @@
-// $(document).ready(function () {
-// $('.field-correct :checkbox').change(function() {
-//     var $check = $(this),
-//         $div = $check.parent();
-//     if ($check.prop('checked')) {
-//          $('[name~="choice"], [name$="correct"]').each(function() {
-//               if (!$(this).is(':checked')) {
-//               $(this).prop('disabled', true);
-//               }
-//               });
-//     } else {
-//          $('[name~="choice"], [name$="correct"]').each(function() {
-//               if (!$(this).is(':checked')) {
-//               $(this).prop('disabled', false);
-//               $(this).css('background-color', 'red');
-//               }
-//               });
-//     }
-// });
-// });
-
-
 // Функция для ограничения выбора только одного верного варианта ответа, если таков вопрос
 $(document).on( 'change', '.field-correct :checkbox', function(){
     var $check = $(this),
@@ -40,4 +18,24 @@ $(document).on( 'change', '.field-correct :checkbox', function(){
     }
 } );
 
+$(document).on('change', '#id_type', function(){
+    var $check = $('.field-correct :checked').prop('checked');
+    if ($check && $('#id_type').val() === '1') {
+         $('[name~="choice"], [name$="correct"]').each(function() {
+              if (!$(this).is(':checked')) {
+              $(this).prop('disabled', true);
+              }
+              });
+    }
+} );
 
+$(document).ready(function(){
+    var $check = $('.field-correct :checked').prop('checked');
+        if ($check && $('#id_type').val() === '1') {
+         $('[name~="choice"], [name$="correct"]').each(function() {
+              if (!$(this).is(':checked')) {
+              $(this).prop('disabled', true);
+              }
+              });
+    }
+} );
